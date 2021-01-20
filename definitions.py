54d95ed8f6ec1458bba260dfeecedf8727a1c28f
paulatoledo30 <43778608+paulatoledo30@users.noreply.github.com>
2021-01-20 11:41:41 -0300

    comentarios

--- definitions.py
+++ definitions.py
@@ -9,29 +9,32 @@ class Agent():
             env: a reference to an environment
         """
         self.env = env
+
     def act(self):
         """
         Defines the agent action
 
         Raises:
-            NotImplementedError:If the method ir not implemented or not overidden.
+            NotImplementedError:If the method is not implemented or not overidden.
         """
         raise NotImplementedError('act')
 
 class Environment:
+    """
+    Implements the interface for an Environment
+    """
     def initial_percepts(self):
         """
         Returns the environment initial percepts
         Raises:
-            NotImplementedError: If the method ir not implemented or not overidden.
+            NotImplementedError: If the method is not implemented or not overidden.
         """
         raise NotImplementedError('initial_percepts')
 
     def signal(self,action):
-         """
+        """
         Returns the environment initial percepts
         Raises:
-            NotImplementedError: If the method ir not implemented or not overidden.
+            NotImplementedError: If the method is not implemented or not overidden.
         """
         raise NotImplementedError('signal')
-
